7326ab310926ec92c0b687e731f23f7e37402a76
matiasgelpi <129074171+matiasgelpi@users.noreply.github.com>
2024-07-03 12:20:11 -0300

    form validation added with zod

---
+++ src/app/lib/formValidation.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { z } from "zod";
+
+const schema = z
+  .object({
+    mail: z
+      .string({
+        invalid_type_error: "Email inválido",
+        required_error: "Email es requerido",
+      })
+      .email({
+        message: "Email inválido",
+      }),
+    name: z
+      .string({
+        invalid_type_error: "Nombre inválido",
+        required_error: "Nombre es requerido",
+      })
+      .min(3, "Un nombre válido es requerido (mínimo 3 caracteres)"),
+    message: z
+      .string({
+        required_error: "Un mensaje es requerido",
+      })
+      .min(10, "Un mensaje válido es requerido (mínimo 10 caracteres)"),
+  })
+  .required();
+
+export async function sendMessage(prevState: any, formData: FormData) {
+  const validatedFields = schema.safeParse({
+    mail: formData.get("mail"),
+    name: formData.get("name"),
+    message: formData.get("message"),
+  });
+  //   console.log(formData);
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors);
+    const errors = validatedFields.error.flatten().fieldErrors;
+    return {
+      message: errors?.message ? errors.message[0] : "",
+      name: errors?.name ? errors.name[0] : "",
+      mail: errors?.mail ? errors.mail[0] : "",
+    };
+  }
+}
--- src/components/contact-form/ContactForm.tsx
+++ src/components/contact-form/ContactForm.tsx
@@ -1,30 +1,38 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import { bonaNova, openSans } from "../../app/lib/fonts";
+import { sendMessage } from "../../app/lib/formValidation";
 import styles from "./contactForm.module.css";
+
+const initialState = {
+  message: "",
+  mail: "",
+  name: "",
+};
 export default function ContactForm() {
-
-  async function sendMessage(formData: FormData) {
-    "use server";
-
-    console.log(Object.fromEntries(formData));
-
-  }
+  const [state, formAction] = useFormState(sendMessage, initialState);
 
   return (
     <section className={`${styles.container} ${openSans.className} `}>
       <p className={`${styles.title} ${bonaNova.className}`}>Contacto:</p>
       <div className={styles.content}>
-  
         <div className={styles.formContainer}>
-          <form action={sendMessage}>
+          <form action={formAction}>
             <label htmlFor="name">
               Nombre:
               <input type="text" name="name" />
+              <p>{state?.name}</p>
             </label>
             <label htmlFor="mail">
               Mail:
               <input type="text" name="mail" />
+              <p>{state?.mail}</p>
             </label>
-            <textarea name="message" id=""></textarea>
+            <label htmlFor="message">
+              <textarea name="message" id=""></textarea>
+              <p>{state?.message}</p>
+            </label>
             <button type="submit"> Enviar </button>
           </form>
         </div>
